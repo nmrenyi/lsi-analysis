dcbbb4e6a31804cd491e005fd85010468c4c2084
LEGION <nmrenyi@outlook.com>
2021-11-21 09:57:37 +0800

    refactor code for get term doc matrix

--- analysis/svd.py
+++ analysis/svd.py
@@ -7,18 +7,20 @@ def load_stopwords(path=r'./stopwords-zh.txt'):
     with open(path, encoding='utf8', mode='r') as f:
         return f.read().split('\n')
 
-def main():
-    DATA_NAME = 'toy10'
-    DATA_PATH = f'../data/{DATA_NAME}.csv'
-    df = pd.read_csv(DATA_PATH, sep='\t')
-    doc_list = [eval(x) for x in df['text'].tolist()]  # list, shape: [#doc, len_of_each_doc]
+def get_term_doc_matrix(data_name):
+    data_path = f'../data/{data_name}.csv'
+    df = pd.read_csv(data_path, sep='\t')
+    docs = [eval(x) for x in df['text'].tolist()]  # list, shape: [#doc, len_of_each_doc]
     # Chinese Tokenizing: ref: https://zhuanlan.zhihu.com/p/345346156
     # include one character as a token (in Chinese one character could be meaningful)
     vectorizer = CountVectorizer(token_pattern=r"(?u)\b\w+\b", stop_words=load_stopwords(), min_df=2)
 
-    term_doc_sparse = vectorizer.fit_transform([' '.join(x) for x in doc_list])  # shape: [#doc, #term]
-    terms = vectorizer.get_feature_names_out()  # len: #term
+    term_doc_sparse = vectorizer.fit_transform([' '.join(x) for x in docs])  # shape: [#doc, #term]
+    terms = vectorizer.get_feature_names_out().tolist()  # len: #term
+    return term_doc_sparse, terms, docs
 
+def main():
+    term_doc_sparse, terms, docs = get_term_doc_matrix('toy100')
 
 if __name__ == '__main__':
     main()
