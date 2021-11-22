55c7cee45524dd617b2dd0e580b8f628cd0f63ad
renyi <ry18@mails.tsinghua.edu.cn>
2021-11-22 09:18:27 +0800

    add prompt output

--- analysis/svd.py
+++ analysis/svd.py
@@ -17,21 +17,26 @@ def load_stopwords(path=r'./stopwords-zh.txt'):
 
 def get_term_doc_matrix(data_name):
     data_path = f'../data/{data_name}.csv'
+    print('reading file', data_path, file=stderr)
     df = pd.read_csv(data_path, sep='\t')
     docs = [eval(x) for x in df['text'].tolist()]  # list, shape: [#doc, len_of_each_doc]
     # Chinese Tokenizing: ref: https://zhuanlan.zhihu.com/p/345346156
     # include one character as a token (in Chinese one character could be meaningful)
+    print('vectorizing text...', file=stderr)
     vectorizer = CountVectorizer(token_pattern=r"(?u)\b\w+\b", stop_words=load_stopwords(), min_df=2)
 
     doc_term_sparse = vectorizer.fit_transform([' '.join(x) for x in docs])  # shape: [#doc, #term]
     term_doc_sparse = doc_term_sparse.transpose()  # shape: [#term, #doc]
     terms = vectorizer.get_feature_names_out().tolist()  # len: #term
+    print('vectorizing complete', file=stderr)
     return term_doc_sparse, terms, docs
 
 def main():
     term_doc_sparse, terms, docs = get_term_doc_matrix(args.dataset)
+    print('performing svd...', file=stderr)
     term_mat, sigma, doc_mat_T = randomized_svd(term_doc_sparse, n_components=args.dim, random_state=args.random_seed)
     doc_mat = doc_mat_T.transpose()
+    print('svd complete', file=stderr)
     # term_mat.shape: [#term, n_components]
     # doc_mat.shape : [#doc,  n_components]
     # sigma.shape   : [#n_components]
